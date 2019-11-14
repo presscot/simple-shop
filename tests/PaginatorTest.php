c138b9dce02111964fbaf206d423670075c03c60
Patryk Prusek <pprusek@localhost.localdomain>
2019-11-14 12:59:26 +0100

    add simple test to Paginator

---
+++ tests/PaginatorTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pprusek
+ * Date: 14.11.19
+ * Time: 12:36
+ */
+
+namespace App\Tests;
+
+use App\Entity\Product;
+use App\Paginator\Paginator;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
+use PHPUnit\Framework\TestCase;
+
+class PaginatorTest extends TestCase
+{
+    public function testPaginatorWhenArgumentsValid():void{
+
+        $product = new Product();
+        $product->setName("test");
+
+        $query = new class($product){
+            private $product;
+            public function __construct($product){$this->product = $product;}
+            public function getResult(){return [$this->product];}
+        };
+
+        $queryBuilder = $this->createMock(QueryBuilder::class);
+
+        $queryBuilder
+            ->expects($this->any())
+            ->method("getQuery")
+            ->willReturn($query);
+        ;
+
+        $queryBuilder
+            ->expects($this->any())
+            ->method("setMaxResults")
+            ->willReturn($queryBuilder);
+        ;
+        $queryBuilder
+            ->expects($this->any())
+            ->method("setFirstResult")
+            ->willReturn($queryBuilder);
+        ;
+        try{
+            $paginator = new Paginator('test',$queryBuilder,1);
+            $paginator->getPaginate();
+        }catch( \Throwable $e ){
+            $this->fail("");
+        }
+
+        $this->assertTrue(true);
+    }
+}
